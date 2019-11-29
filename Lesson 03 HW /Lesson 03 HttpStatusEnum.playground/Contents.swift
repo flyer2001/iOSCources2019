198713717fff307296ae1ce8a2d169d490e2f4da
Seg <s.popyvanov@gmail.com>
2019-11-30 00:18:28 +0400

    lesson 03 http status done

---
+++ Lesson 03 HW /Lesson 03 HttpStatusEnum.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+struct HTTPStatusCode {
+    var code: Int
+    
+    init(code: Int) {
+        self.code = code
+    }
+    
+    enum DescriptionOfStatus {
+        case badRequest
+        case unauthorized
+        case paymentRequired
+        case forbidden
+        case notFound
+        case methodNotAllowed
+        case notAcceptable
+        case proxyAuthenticationRequired
+        case requestTimeout
+        case conflict
+        case gone
+        case undefine
+        }
+
+    var description: DescriptionOfStatus {
+        switch self.code {
+        case 400:
+            return .badRequest
+        case 401:
+            return .unauthorized
+        case 402:
+            return .paymentRequired
+        case 403:
+            return .forbidden
+        case 404:
+            return .notFound
+        case 405:
+            return .methodNotAllowed
+        case 406:
+            return .notAcceptable
+        case 407:
+            return .proxyAuthenticationRequired
+        case 408:
+            return .requestTimeout
+        case 409:
+            return .conflict
+        case 410:
+            return .gone
+        default:
+            return .undefine
+        }
+    }
+}
+    
+let status = HTTPStatusCode(code: 400)
+print(status.description)
