d974b7ba4279d034f0875a605e41dc4c8db81e55
Seg <s.popyvanov@gmail.com>
2019-12-10 12:55:07 +0400

    поправил задание с enum

--- Lesson 03 HW /Lesson 03 HttpStatusEnum.playground/Contents.swift
+++ Lesson 03 HW /Lesson 03 HttpStatusEnum.playground/Contents.swift
@@ -3,86 +3,50 @@
 import UIKit
 
 
-struct HTTPStatusCode {
-    var code: Int
-    
-    init(code: Int) {
-        self.code = code
-    }
-    
-    enum DescriptionOfStatus {
-        case badRequest
-        case unauthorized
-        case paymentRequired
-        case forbidden
-        case notFound
-        case methodNotAllowed
-        case notAcceptable
-        case proxyAuthenticationRequired
-        case requestTimeout
-        case conflict
-        case gone
-        case undefine
+enum HTTPStatusCode {
+    case badRequest
+    case unauthorized
+    case paymentRequired
+    case forbidden
+    case notFound
+    case methodNotAllowed
+    case notAcceptable
+    case proxyAuthenticationRequired
+    case requestTimeout
+    case conflict
+    case gone
+    case undefine
         
     var errorMsg: String {
         switch self {
-        case .badRequest:
-            return "Bad Request"
-        case .unauthorized:
-            return "Unauthorized"
-        case .paymentRequired:
-            return "Payment Required"
-        case .forbidden:
-            return "Forbidden"
-        case .notFound:
-            return "notFound"
-        case .methodNotAllowed:
-            return "Method not allowed"
-        case .notAcceptable:
-            return "Not acceptable"
-        case .proxyAuthenticationRequired:
-            return "Proxy authentication required"
-        case .requestTimeout:
-            return "Request timout"
-        case .conflict:
-            return "Conflict"
-        case .gone:
-            return "Gone"
-        case .undefine:
-            return "Undefine"
-            }
-       }
-    }
-
-    var description: DescriptionOfStatus {
-        switch self.code {
-        case 400:
-            return .badRequest
-        case 401:
-            return .unauthorized
-        case 402:
-            return .paymentRequired
-        case 403:
-            return .forbidden
-        case 404:
-            return .notFound
-        case 405:
-            return .methodNotAllowed
-        case 406:
-            return .notAcceptable
-        case 407:
-            return .proxyAuthenticationRequired
-        case 408:
-            return .requestTimeout
-        case 409:
-            return .conflict
-        case 410:
-            return .gone
-        default:
-            return .undefine
-        }
+            case .badRequest:
+                return "400 Bad Request"
+            case .unauthorized:
+                return "401 Unauthorized"
+            case .paymentRequired:
+                return "402 Payment Required"
+            case .forbidden:
+                return "403 Forbidden"
+            case .notFound:
+                return "404 notFound"
+            case .methodNotAllowed:
+                return "405 Method not allowed"
+            case .notAcceptable:
+                return "406 Not acceptable"
+            case .proxyAuthenticationRequired:
+                return "407 Proxy authentication required"
+            case .requestTimeout:
+                return "408 Request timout"
+            case .conflict:
+                return "409 Conflict"
+            case .gone:
+                return "410 Gone"
+            case .undefine:
+                return "411 Undefine"
+                }
     }
 }
-    
-let status = HTTPStatusCode(code: 400)
-print(status.description.errorMsg)
+
+       
+let status = HTTPStatusCode.badRequest.errorMsg
+
